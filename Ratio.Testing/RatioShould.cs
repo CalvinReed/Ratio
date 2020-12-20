0ad3d51e27bebb83b8d7ef4bf7f6aa4985cda8e3
Calvin Reed <calvinreed@posteo.net>
2020-12-20 10:55:20 -0500

    Added new reduction test

--- Ratio.Testing/RatioShould.cs
+++ Ratio.Testing/RatioShould.cs
@@ -5,6 +5,12 @@ namespace CalvinReed.Testing
 {
     public class RatioShould
     {
+        [Fact]
+        public void HaveCorrectDefault()
+        {
+            Assert.Equal(0, default(Ratio));
+        }
+
         [Theory]
         [MemberData(nameof(AdditionData))]
         public void AddCorrectly(Ratio a, Ratio b, Ratio c)
@@ -12,17 +18,29 @@ namespace CalvinReed.Testing
             Assert.Equal(c, a + b);
         }
 
-        [Fact]
-        public void HaveCorrectDefault()
+        [Theory]
+        [MemberData(nameof(ReductionData))]
+        public void ReduceCorrectly(Ratio expected, Ratio actual)
         {
-            Assert.Equal(0, default(Ratio));
+            Assert.Equal(expected, actual);
         }
 
         public static IEnumerable<object[]> AdditionData()
         {
             yield return new object[] {Ratio.Create(1, 3), Ratio.Create(1, 3), Ratio.Create(2, 3)};
+            yield return new object[] {Ratio.Create(1, 2), Ratio.Create(1, 3), Ratio.Create(5, 6)};
             yield return new object[] {Ratio.Create(1, 3), Ratio.Create(-1, 3), Ratio.Create(0, 1)};
             yield return new object[] {Ratio.Create(9, 7), Ratio.Create(-2, 7), Ratio.Create(1, 1)};
+        }
+
+        public static IEnumerable<object[]> ReductionData()
+        {
+            yield return new object[] {Ratio.Create(1, 3), Ratio.Create(-1, -3)};
+            yield return new object[] {Ratio.Create(1, 3), Ratio.Create(2, 6)};
+            yield return new object[] {Ratio.Create(1, 3), Ratio.Create(-2, -6)};
+            yield return new object[] {Ratio.Create(-1, 3), Ratio.Create(1, -3)};
+            yield return new object[] {Ratio.Create(-1, 3), Ratio.Create(-2, 6)};
+            yield return new object[] {Ratio.Create(-1, 3), Ratio.Create(2, -6)};
         }
     }
 }
